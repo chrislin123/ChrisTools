7783528d5d0a83a6f853e984a81ae01b5dbe491f
chrislin123 <chris.lin.tw@hotmail.com>
2015-07-27 13:24:33 +0800

    1040727 C修改，加入判斷資料夾大於4g的檔案

---
+++ CheckOver4gFile/CheckOver4g.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace CheckOver4gFile
+{
+    partial class CheckOver4g
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(695, 8);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(6);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(138, 46);
+            this.btnStart.TabIndex = 10;
+            this.btnStart.Text = "啟動";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 19);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "解析路徑：";
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Location = new System.Drawing.Point(149, 16);
+            this.txtFrom.Margin = new System.Windows.Forms.Padding(6);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(384, 22);
+            this.txtFrom.TabIndex = 8;
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Location = new System.Drawing.Point(545, 8);
+            this.btnBrowser.Margin = new System.Windows.Forms.Padding(6);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(138, 46);
+            this.btnBrowser.TabIndex = 7;
+            this.btnBrowser.Text = "瀏覽";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.ItemHeight = 12;
+            this.lstFiles.Location = new System.Drawing.Point(34, 60);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(799, 232);
+            this.lstFiles.TabIndex = 12;
+            // 
+            // CheckOver4g
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 352);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.btnBrowser);
+            this.Name = "CheckOver4g";
+            this.Text = "判斷資料夾中大於4g的檔案功能";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox lstFiles;
+    }
+}
+
