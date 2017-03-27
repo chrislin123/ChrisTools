88e43f44a795a90751589e3674f05fe3c9051ddb
chrislin123 <chris.lin.tw@hotmail.com>
2017-03-27 16:44:25 +0800

    1060327_c_新增MegaRouting專案

---
+++ MegaRouting/Tools001.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CG.Web.MegaApiClient;
+
+namespace MegaRouting
+{
+  public partial class Tools001 : Form
+  {
+    public Tools001()
+    {
+      InitializeComponent();
+    }
+
+    private void btnStart_Click(object sender, EventArgs e)
+    {
+      for (int i = 0; i < 40; i++)
+      {
+        MegaApiClient client = new MegaApiClient();
+
+        int idx = i + 1;
+        string sEmail = string.Format("chris.lin.tw1234+{0}@gmail.com", idx.ToString().PadLeft(3, '0'));
+
+        showMsg(string.Format("{0}==登入中", sEmail));        
+        client.Login(sEmail, "fm719531");
+        showMsg(string.Format("{0}==登入成功", sEmail));
+        
+        showMsg(string.Format("{0}==讀取中", sEmail));
+        var nodes = client.GetNodes();
+        showMsg(string.Format("{0}==讀取成功", sEmail));
+        
+        showMsg(string.Format("{0}==登出中", sEmail));
+        client.Logout();
+        showMsg(string.Format("{0}==登出成功", sEmail));
+        
+        showMsg("");
+        
+      }
+
+
+      return;
+
+
+      //INode root = nodes.Single(n => n.Type == NodeType.Root);
+      //INode myFolder = client.CreateFolder("Upload", root);
+
+      //INode myFile = client.UploadFile("MyFile.ext", myFolder);
+
+      //Uri downloadUrl = client.GetDownloadLink(myFile);
+      //Console.WriteLine(downloadUrl);
+      
+    }
+
+
+    private void showMsg(string pMsg)
+    {
+      richTextBox1.AppendText(pMsg + Environment.NewLine);
+      richTextBox1.ScrollToCaret();
+      richTextBox1.Refresh();
+
+    }
+  }
+}
